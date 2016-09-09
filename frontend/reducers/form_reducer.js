53486f0e6b967b9a47aa8dc6c5f1c41a6659742a
Joshua Yang <tianjyang@gmail.com>
2016-09-09 12:44:23 -0700

    set a default center location to san francisco in the search params

--- frontend/reducers/form_reducer.js
+++ frontend/reducers/form_reducer.js
@@ -1,7 +1,7 @@
 import { merge } from 'lodash';
 import { SEARCH_CONSTANTS } from '../actions/search_actions';
 
-const formReducer = (state = {}, action) => {
+const formReducer = (state = {location: "San Francisco"}, action) => {
   switch (action.type) {
     case SEARCH_CONSTANTS.UPDATE_SEARCH_PARAMS:
       return merge({},state,action.searchParams);
