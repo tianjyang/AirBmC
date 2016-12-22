569903d581004f80f9f76b75337c3ff818db64a5
Joshua Yang <tianjyang@gmail.com>
2016-12-21 22:10:31 -0800

    tiny tweak

--- frontend/components/map/map_container.jsx
+++ frontend/components/map/map_container.jsx
@@ -11,7 +11,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  searchByBounds: (searchParams) => dispatch(searchByBounds(searchParams)),
+  searchByBounds: (searchParams) => dispatch(searchByBounds(searchParams));
 });
 
 export default connect(
